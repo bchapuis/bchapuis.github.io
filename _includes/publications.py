19fd73143c99ddeae2087c512ea46e7dff536cb3
Bertil Chapuis <bchapuis@gmail.com>
2019-01-05 22:56:34 +0100

    fixed references

--- _includes/publications.py
+++ _includes/publications.py
@@ -10,11 +10,12 @@ sorted(data, key=lambda k: k['meta']['parsedDate'])
 with open('publications.html', 'w') as f:
 	for p in data:
 		if 'proceedingsTitle' in p['data']:
+			key = p['data']['key']
 			authors = ", ".join('{0} {1}'.format(a['firstName'], a['lastName']) for a in p['data']['creators']).replace('Bertil Chapuis', '<b>Bertil Chapuis</b>')
 			title = p['data']['title']
 			proceedings = p['data']['proceedingsTitle']
 			pages = p['data']['pages']
 			doi = p['data']['DOI']
+			isbn = p['data']['ISBN']
 			date = p['meta']['parsedDate']
-
-			print(f'<li>{authors}. <a href="https://doi.org/{doi}"><b>{title}</b></a>. {proceedings}, {pages}.</li>', file=f)
+			print(f'<li>{authors}. <a href="https://doi.org/{doi}"><b>{title}</b></a>. {proceedings}, {pages}, <a href="/pubs/{key}.pdf" class="uk-link-text uk-icon-link" uk-icon="file-pdf" target="_blank"></a>.</li>', file=f)
